6f7279562b69ae75882bbfafc9490ca3a2c3f8a3
ozan-kulug <ozankulug12@gmail.com>
2026-08-25 15:09:47 +0200

    etape 1

---
+++ script.js
@@ -0,0 +1,9 @@
+/**
+ * Fichier JavaScript pour l'application PokeCount.
+ * @author Steve Fallet <steve.fallet@divtec.ch>
+ * @version 0.1 (Version actuelle)
+ * @since 2024-01-31 (Date de création)
+ */
+
+"use strict"; // Interprètation stricte du code
+alert("Hello World"); // Affiche "Hello World" dans la console
